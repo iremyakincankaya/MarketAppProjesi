3964a0e27b69b3977bd84a9ff659a6d7c79dce97
iremyakincankaya <yakincanirem@gmail.com>
2025-01-07 20:36:15 +0300

    migrations

--- MarketApp.API/Program.cs
+++ MarketApp.API/Program.cs
@@ -1,8 +1,12 @@
+using MarketApp.DAL.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ApplicationDbContext>(o=>o.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContext")));
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ MarketApp.DAL/Context/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using MarketApp.DAL.Context.Configuration;
+using MarketApp.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketApp.DAL.Context
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) 
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //  ayrı bir klasör içinde göstericez
+            //  modelBuilder.Entity<Category>().Property(c=>c.Name).HasMaxLength(100);
+            //ayrı ayrı ekleme
+            // modelBuilder.ApplyConfiguration < new CategoryConfiguration() >;
+
+            //tüm confguration dosyalarını tara ve entegre at
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+
+        }
+    }
+}
